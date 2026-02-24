b42585a59cb3e92e7840b11c335778275425015b
JoranSt <joran.stavenuiter@hva.nl>
2026-02-24 20:10:10 +0100

    Removed unneeded comments

--- Python files/chess.py
+++ Python files/chess.py
@@ -1,11 +1,7 @@
 from chessPieces import *
 from board import Board
 import os
-#Om te kijken of de koning schaak staat maak ik een functie aan die na het bewegen de valid_moves method aanroept
-# Die functie past dan of de check attribute aan of kijkt naar of de koning op een valid_moves vlak staat
-# Ook kan je voor elke beurt checken of the koning schaak staat door te kijken welke stukken die tegenkomt. 
-#Bijvoorbeeld, koning h5 niks tussen h5 en h1 dus staat hij schaak (aannemend dat het verschillende kleuren zijn)
-# vervolgens check alle stukkune of die schaak zetten en bepaal dan welke moves mogelijk zijn om uit schaak te komen indien geen moves schaakmat
+
 board = Board()
 
 
@@ -13,6 +9,7 @@ class Chess:
     def __init__(self, board, turn = "white"):
         self.board = board
         self.turn = turn
+        os.system('cls')
         self.board.display_board()
     
     def play(self):
@@ -20,10 +17,6 @@ class Chess:
                 
             
             
-            #checks if the side whos turn it is is checked
-            # check = self.board.is_check(self.turn)
-            # print(check)
-            #check if king is checked
             move = input("Enter move in the form of B1D1\n")
             movesplit = list(move)
             #splits the input and gives itto the move piece function in the board class
@@ -35,6 +28,8 @@ class Chess:
             except:
                 print("Input unvalid please try again")
                 continue
+            
+            
             messages = {
                         "no_piece": "No piece at that square.",
                         "wrong_turn": "That’s not your piece.",
@@ -54,12 +49,12 @@ class Chess:
                         break
                     color, piece = self.board.piece_info([coordinates[2],coordinates[3]])
                     from_, to_ = movesplit[:2], movesplit[2:]
-                    #os.system('cls')
+                    os.system('cls')
                     print(f"{color} {piece} moved from {from_[0]}{from_[1]} to {to_[0]}{to_[1]}")
                     print("\n")
                     
             else:
-                #os.system('cls')
+                os.system('cls')
                 print(messages.get(result, "Unknown error"))
 
             self.board.display_board()
